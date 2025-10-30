f27079653e8d12d70bc0e6c46cffea9c1bd41a31
Papitha0525 <mpapitha2005@gmail.com>
2025-10-30 09:50:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Alert when user downloads resume
+document.querySelector('.secondary').addEventListener('click', () => {
+  alert('Your resume download will start shortly!');
+});
